afa539636fcb5d71b35cc900309c595f48f61395
Yanko Dimitrov <yanko@yankodimitrov.com>
2016-02-06 13:38:58 +0200

    Added KeychainItemType protocol

---
+++ Keychain/KeychainItemType.swift
@@ -0,0 +1,19 @@
+//
+//  KeychainItemType.swift
+//  Keychain
+//
+//  Created by Yanko Dimitrov on 2/6/16.
+//  Copyright © 2016 Yanko Dimitrov. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - KeychainItemType
+
+public protocol KeychainItemType {
+    
+    var accessMode: String {get}
+    var attributes: [String: AnyObject] {get}
+    var data: [String: AnyObject] {get set}
+    var dataToStore: [String: AnyObject] {get}
+}
---
+++ KeychainTests/KeychainTests.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainTests.swift
+//  KeychainTests
+//
+//  Created by Yanko Dimitrov on 2/6/16.
+//  Copyright © 2016 Yanko Dimitrov. All rights reserved.
+//
+
+import XCTest
+@testable import Keychain
+
+class KeychainTests: XCTestCase {
+    
+}
