75a23b3dbab4162398c8015375fdddd7479a92a9
Paul Wilkinson <paulw@wilko.me>
2017-09-21 11:55:09 +1000

    Add default initialiser for Swift 4 compatibility

--- Keychain/Keychain.swift
+++ Keychain/Keychain.swift
@@ -113,6 +113,10 @@ extension KeychainGenericPasswordType {
 
 public struct Keychain: KeychainServiceType {
     
+    // Default initializer
+    
+    public init() {}
+    
     internal func errorForStatusCode(_ statusCode: OSStatus) -> NSError {
         
         return NSError(domain: "swift.keychain.error", code: Int(statusCode), userInfo: nil)
